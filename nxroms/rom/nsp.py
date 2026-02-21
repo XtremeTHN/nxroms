996c39e1d7ea69e911d20b1ab69109f7822fbeff
Axel <nigthmaresans2@gmail.com>
2026-02-21 13:28:40 -0600

    fix: reading from pathlib.Path

--- nxroms/rom/nsp.py
+++ nxroms/rom/nsp.py
@@ -1,13 +1,15 @@
 from ..fs.pfs0 import PFS0
-from ..readers import File
+from ..readers import File, IReadable
 from ..nca.nca import Nca
+from pathlib import Path
 import os
 
 
 class Nsp(PFS0):
-    def __init__(self, file: str):
-        f = File(file)
-        super().__init__(f)
+    def __init__(self, rom: IReadable | File | Path | str):
+        if isinstance(rom, str) or isinstance(rom, Path):
+            rom = File(rom)
+        super().__init__(rom)
 
     def get_file(self, index: int) -> Nca | None:
         if os.path.splitext(self.header.file_entry_table[index].name)[1] != ".nca":
